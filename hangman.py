a611e23edf020b5ba898ad3d7d70593cbf8fbcbd
Yunus Ozdemir <31583488+yunusozde@users.noreply.github.com>
2018-07-11 15:56:56 -0500

    Summary

---
+++ hangman.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import random
+ 
+def winner(players=('Amy', 'Bill', 'Cathy', 'Dale')):
+
+    winner = random.choice(players) 
+
+    ####
+    # Summarize the following section of code here
+    ####
+    print('Guess which of these people won the lottery: ',end='')
+    for p in players[:len(players)-1]: # explain index here
+        print(p+', ', end='')
+    print(players[len(players)-1]) # explain this line here
+
+    ####
+    # Summarize the following section of code here
+    ####
+    guesses = 1 
+    while raw_input() != winner:
+        print('Guess again!')
+        guesses += 1
+    print('You guessed in', guesses, 'guesses!')
+    return guesses
